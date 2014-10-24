59033dc1bd564a9ee0ab39fe30457a34d94479d7
KBOB <Konjkov.VV@gmail.com>
2014-10-24 19:21:24 +0700

    add mp2 tests

--- tests/test_mp2.py
+++ tests/test_mp2.py
@@ -1,13 +1,16 @@
 import unittest, logging
 from pyquante2.geo.molecule import read_xyz
-from pyquante2 import rhf,basisset,h2,mp2
+from pyquante2 import rhf, basisset, h2, lih, ch4, h2o, mp2
 
 
 HBr = read_xyz('./molfiles/HBr.xyz')
 
+H2O4 = read_xyz('./molfiles/H2O_4.xyz')
+
+N8 = read_xyz('./molfiles/N8.xyz')
 
 class PyQuanteAssertions:
-    def assertPrecisionEqual(self, a, b, prec=2e-5):
+    def assertPrecisionEqual(self, a, b, prec=3e-4):
         x = abs(2*(a-b)/(a+b))
         if x > prec:
             raise AssertionError("%.9f is equal %.9f with precision %.9f)" % (a, b, x))
@@ -19,16 +22,48 @@ class test_mp2(unittest.TestCase, PyQuanteAssertions):
         solver=rhf(h2,bfs)
         solver.converge()
         nvirt = len(bfs)-h2.nocc()
-        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,h2.nocc(),len(bfs)-h2.nocc())
+        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,h2.nocc(),nvirt)
         self.assertPrecisionEqual(emp2, -0.026304104341)
+
+    def test_LiH(self):
+        bfs = basisset(lih,'cc-pvdz')
+        solver=rhf(lih, bfs, libint=True)
+        solver.converge()
+        nvirt = len(bfs)-lih.nocc()
+        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,lih.nocc(),nvirt)
+        self.assertPrecisionEqual(emp2, -0.023948620832)
+
+    def test_H2O(self):
+        bfs = basisset(h2o,'cc-pvdz')
+        solver=rhf(h2o, bfs, libint=True)
+        solver.converge()
+        nvirt = len(bfs)-h2o.nocc()
+        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,h2o.nocc(),nvirt)
+        self.assertPrecisionEqual(emp2, -0.206440187835)
+
+    def test_CH4(self):
+        bfs = basisset(ch4,'cc-pvdz')
+        solver=rhf(ch4, bfs, libint=True)
+        solver.converge()
+        nvirt = len(bfs)-ch4.nocc()
+        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,ch4.nocc(),nvirt)
+        self.assertPrecisionEqual(emp2, -0.166640105042)
 
     def test_HBr(self):
         bfs = basisset(HBr,'cc-pvdz')
-        solver=rhf(HBr,bfs,libint=True)
+        solver=rhf(HBr, bfs, libint=True)
         solver.converge()
-        nvirt = len(bfs)-h2.nocc()
-        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,h2.nocc(),len(bfs)-h2.nocc())
-        self.assertPrecisionEqual(emp2, -0.1532843)
+        nvirt = len(bfs)-HBr.nocc()
+        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,HBr.nocc(),nvirt)
+        self.assertPrecisionEqual(emp2, -0.153284373119)
+
+    def test_N8(self):
+        bfs = basisset(N8,'cc-pvdz')
+        solver=rhf(N8, bfs, libint=True)
+        solver.converge()
+        nvirt = len(bfs)-N8.nocc()
+        emp2 = mp2(solver.i2,solver.orbs,solver.orbe,N8.nocc(),nvirt)
+        self.assertPrecisionEqual(emp2, -1.328348475507)
 
 
 def runsuite(verbose=True):
