2b722999a6dc9a3e06aa236d48200e0301e92471
KBOB <Konjkov.VV@gmail.com>
2014-11-04 01:11:34 +0700

    implement ROHF

--- pyquante2/scf/iterators.py
+++ pyquante2/scf/iterators.py
@@ -1,4 +1,4 @@
-from guess import core_hamiltonian_rhf, core_hamiltonian_uhf
+from guess import core_hamiltonian_rhf, core_hamiltonian_uhf, core_hamiltonian_rohf
 
 
 class SCFIterator(object):
@@ -12,10 +12,9 @@ class SCFIterator(object):
         D = self.D
         for iteration in range(maxiters):
             """Update orbital energies and eigenvectors"""
-            F = self.H.fock(D)
+            (F, E), Eold = self.H.fock_energy(D), E
             orbe, orbs = self.H.eigenv(F)
             D = self.H.density(orbs)
-            E, Eold = self.H.energy(D, F), E
             if abs(E-Eold) < tol:
                 self.D = D
                 self.orbe, self.orbs = orbe, orbs
@@ -35,14 +34,35 @@ class USCFIterator(object):
         Da = self.Da
         Db = self.Db
         for iteration in range(maxiters):
-            Fa, Fb = self.H.fock(Da, Db)
+            (Fa, Fb, E), Eold = self.H.fock_energy(Da, Db), E
             (orbea, orbsa), (orbeb, orbsb) = self.H.eigenv(Fa, Fb)
             Da, Db = self.H.density(orbsa, orbsb)
-            E, Eold = self.H.energy(Da, Db, Fa, Fb), E
             if abs(E-Eold) < tol:
                 self.Da, self.Db = Da, Db
                 self.orbea, self.orbsa = (orbea, orbsa)
                 self.orbeb, self.orbsb = (orbeb, orbsb)
+                self.energy = E
+                self.converged = True
+                break
+
+
+class ROSCFIterator(object):
+    def __init__(self, H, guess=core_hamiltonian_rohf):
+        self.H = H
+        self.Da, self.Db = guess(H)
+        self.converged = False
+
+    def converge(self, tol=1e-7, maxiters=100):
+        E = 0
+        Da = self.Da
+        Db = self.Db
+        for iteration in range(maxiters):
+            (Feff, E), Eold = self.H.fock_energy(Da, Db), E
+            orbe, orbs = self.H.eigenv(Feff)
+            Da, Db = self.H.density(orbs)
+            if abs(E-Eold) < tol:
+                self.Da, self.Db = Da, Db
+                self.orbe, self.orbs = (orbe, orbs)
                 self.energy = E
                 self.converged = True
                 break
@@ -53,11 +73,10 @@ class AveragingIterator(SCFIterator):
         E = 0
         D = Dold = self.D
         for iteration in range(maxiters):
-            F = self.H.fock(D)
+            (F, E), Eold = self.H.fock_energy(D), E
             orbe, orbs = self.H.eigenv(F)
             D = self.H.density(orbs)
             D, Dold = (1-fraction)*Dold + fraction*D, D
-            E, Eold = self.H.energy(D, F), E
             if abs(E-Eold) < tol:
                 self.D = D
                 self.orbe, self.orbs = orbe, orbs
